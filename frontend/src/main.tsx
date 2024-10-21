3b10d44f8985997f954a900c2f5a48f92c03f7fe
jeongbaebang <jeongbaebang_dev@naver.com>
2024-10-21 23:06:47 +0900

    feat: 프로젝트 기본 router 구성 (#1)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
+import ErrorPage from "routes/ErrorPage.tsx";
+import HomePage from "routes/HomePage.tsx";
+import NotFoundPage from "routes/NotFoundPage.tsx";
+
+const router = createBrowserRouter([
+  // 메인 페이지
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  // 404 페이지
+  {
+    path: "/*",
+    element: <NotFoundPage />,
+  },
+]);
 
 const enableMocking = async () => {
   if (process.env.NODE_ENV !== "development" || import.meta.env.VITE_MSW_ENABLED !== "true") {
     return;
   }
 
-  const { worker } = await import("./mocks/browser");
+  const { worker } = await import("./mocks/browser.ts");
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
@@ -18,7 +35,7 @@ const enableMocking = async () => {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 });
---
+++ frontend/src/routes/ErrorPage.tsx
@@ -0,0 +1,20 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>Oops! something is wrong</h1>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+      </div>
+    );
+  } else {
+    return <div>Oops! something is wrong</div>;
+  }
+};
+
+export default ErrorPage;

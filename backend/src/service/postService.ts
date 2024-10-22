22c0f66b783ff2d2fc7e0f8c2bcb65cf9aaa0ea3
jeongbaebang <jeongbaebang_dev@naver.com>
2024-10-22 15:31:43 +0900

    feat: 포스트 API 작업 (#3)
    - 포스트 모델 스키마 정의
    - GET /posts
    - POST /posts

---
+++ backend/src/service/postService.ts
@@ -0,0 +1,40 @@
+import Post, { PostSchemaType } from "@src/models/postModel";
+
+export interface IPostService {
+  createUser(data: PostSchemaType): Promise<PostSchemaType>;
+  getPosts(): Promise<ExtendedPostSchemaType[]>;
+}
+
+// TODO: 타입 분리
+interface ExtendedPostSchemaType extends PostSchemaType {
+  category: {
+    categoryId: string;
+    name: string;
+  }[];
+}
+
+export class PostService implements IPostService {
+  async createUser(data: PostSchemaType): Promise<PostSchemaType> {
+    const userData = new Post(data);
+
+    return await userData.save();
+  }
+
+  // TODO: 사용자 구분하기
+  // TODO: 카테고리 참조
+  async getPosts(): Promise<ExtendedPostSchemaType[]> {
+    const posts = await Post.find().lean();
+    const mappedPosts = posts.map((post) => ({
+      postId: post._id,
+      title: post.title,
+      content: post.content,
+      images: post.images,
+      createdAt: post.createdAt,
+      updateAt: post.updatedAt,
+      authorId: post.authorId,
+      category: [],
+    }));
+
+    return mappedPosts;
+  }
+}
--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 export type UserSchemaType = {
   name: string;
   address: string;
@@ -5,3 +7,10 @@ export type UserSchemaType = {
   description: string;
   isCompleted?: boolean;
 };
+
+export interface IController {
+  create(req: Request, res: Response): Promise<void>;
+  read(req: Request, res: Response): Promise<void>;
+  update(req: Request, res: Response): Promise<void>;
+  delete(req: Request, res: Response): Promise<void>;
+}

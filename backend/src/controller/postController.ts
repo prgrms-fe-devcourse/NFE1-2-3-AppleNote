ff33541113033480831e99abee7fc16f81fb9dc5
jeongbaebang <jeongbaebang_dev@naver.com>
2024-10-23 11:51:19 +0900

    feat: 포스트 삭제 API (#3)
    - 누락된 컨트롤러 추가

--- backend/src/controller/postController.ts
+++ backend/src/controller/postController.ts
@@ -1,36 +1,52 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response } from "express";
 
 import { PostSchemaType } from "@src/models/postModel";
 import { IPostService } from "@src/service/postService";
 import { IController } from "@src/types";
 import { createErrorResponse, createSuccessResponse } from "@src/utils/createError";
+import { PostError } from "@src/utils/Error";
 
 export class PostController implements IController {
   constructor(private postService: IPostService) {}
 
-  async create(req: Request, res: Response): Promise<void> {
+  async create(req: Request, res: Response) {
     try {
       const postData: PostSchemaType = req.body;
       const post = await this.postService.createPost(postData);
 
-      res.status(201).json(createSuccessResponse(201, post));
+      return res.status(201).json(createSuccessResponse(201, post));
     } catch {
-      res.status(500).json(createErrorResponse(500, "Internal server error"));
+      return res.status(500).json(createErrorResponse(500, "Internal server error"));
     }
   }
 
-  async read(_req: Request, res: Response): Promise<void> {
+  async read(_req: Request, res: Response) {
     try {
       const posts = await this.postService.getPosts();
 
-      res.status(200).json(createSuccessResponse(200, posts));
+      return res.status(200).json(createSuccessResponse(200, posts));
     } catch {
-      res.status(500).json(createErrorResponse(500, "Internal server error"));
+      return res.status(500).json(createErrorResponse(500, "Internal server error"));
     }
   }
 
-  async update(_req: Request, _res: Response): Promise<void> {}
+  async update(_req: Request, res: Response) {
+    return res.status(200).json(createSuccessResponse(200, { good: true }));
+  }
 
-  async delete(_req: Request, _res: Response): Promise<void> {}
+  async delete(req: Request, res: Response) {
+    try {
+      const postId = req.params.postId;
+
+      await this.postService.deletePost(postId);
+
+      return res.status(200).json(createSuccessResponse(200, { isRemove: true }));
+    } catch (error) {
+      if (error instanceof PostError) {
+        return res.status(404).json(createErrorResponse(404, error.message));
+      }
+
+      return res.status(500).json(createErrorResponse(500, "Internal server error"));
+    }
+  }
 }

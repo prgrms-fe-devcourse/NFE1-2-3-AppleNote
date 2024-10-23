db8a73a9d46f4aa10e28b254282f8b71748b5486
jiyoung <jyhan0309@naver.com>
2024-10-23 14:37:14 +0900

    feat: 본문, 본문 textarea (Type 추가, resize 막음) 추가 완료 (#5)

--- frontend/src/components/post/CreatePostPage.tsx
+++ frontend/src/components/post/CreatePostPage.tsx
@@ -3,17 +3,26 @@ import styled from "styled-components";
 
 const CreatePostPage: React.FC = () => {
   const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
 
   // eslint-disable-next-line no-console
-  console.log(title);
+  console.log(title, content);
 
   return (
     <Wrapper>
       <Title>제목</Title>
       <TitleInput onChange={(e) => setTitle(e.target.value)} type="text" />
+      <Title>본문</Title>
+      <ContentText onChange={(e) => setContent(e.target.value)} />
     </Wrapper>
   );
 };
+
+const ContentText = styled.textarea`
+  width: 100%;
+  height: 300px;
+  resize: none;
+`;
 
 const TitleInput = styled.input`
   width: 100%;

92f24552199414e7573da925cb9130ac86bbda48
jiyoung <jyhan0309@naver.com>
2024-10-23 12:57:40 +0900

    feat: 제목, 제목 input (Type 추가해서) 추가 완료 (#5)

--- frontend/src/components/post/CreatePostPage.tsx
+++ frontend/src/components/post/CreatePostPage.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const CreatePostPage = () => {
-  return <div></div>;
+const CreatePostPage: React.FC = () => {
+  const [title, setTitle] = useState<string>("");
+
+  // eslint-disable-next-line no-console
+  console.log(title);
+
+  return (
+    <Wrapper>
+      <Title>제목</Title>
+      <TitleInput onChange={(e) => setTitle(e.target.value)} type="text" />
+    </Wrapper>
+  );
 };
+
+const TitleInput = styled.input`
+  width: 100%;
+`;
+const Title = styled.div`
+  font-size: 20px;
+  color: gray;
+`;
+const Wrapper = styled.div`
+  width: 600px;
+`;
 
 export default CreatePostPage;

1b4755753416ae93a76c989a0ae819d73bd5add8
sunglitter <kds0284@gmail.com>
2024-10-23 16:26:30 +0900

    feat: 배너 영역 구현 (#6)

--- frontend/src/components/main/HomePage.tsx
+++ frontend/src/components/main/HomePage.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import useCustomTheme from "@common/styles/useCustomTheme";
 import Category from "@components/category/Category";
 import Header from "@components/main/Header";
+import Banner from "@components/main/Banner";
 
 interface HomePageProps {
   data?: null;
@@ -20,6 +21,7 @@ const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div>
       <Header />
+      <Banner />
       <h1>
         {icon} Hello World {icon}
       </h1>

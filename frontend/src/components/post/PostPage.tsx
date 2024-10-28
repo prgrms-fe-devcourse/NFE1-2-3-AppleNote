6716f196c894375e325d0e2933279860b989554a
jiyoung <jyhan0309@gmail.com>
2024-10-28 14:40:48 +0900

    feat: post 페이지 API 호출 (#5)

--- frontend/src/components/post/PostPage.tsx
+++ frontend/src/components/post/PostPage.tsx
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
+import { fetchPost } from "./postAPI";
+import { useEffect } from "react";
 
 const PostPage = () => {
   const { id } = useParams<{ id: string }>();
+  const fetchPostData = async () => {
+    try {
+      const data = await fetchPost(id as string);
 
-  console.log(id);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostData();
+  }, []);
 
   return <></>;
 };

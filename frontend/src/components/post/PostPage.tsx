7578478f4023a2ede9cecce6c19e19f7e4ffeb6c
jiyoung <jyhan0309@gmail.com>
2024-10-28 16:31:52 +0900

    feat: post 상세페이지 임시 완성 (#5)

--- frontend/src/components/post/PostPage.tsx
+++ frontend/src/components/post/PostPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
-import { fetchPost } from "./postAPI";
 import { useEffect } from "react";
+import { fetchPost } from "./postAPI";
+import styled from "styled-components";
 
 const PostPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -18,7 +19,45 @@ const PostPage = () => {
     fetchPostData();
   }, []);
 
-  return <></>;
+  return (
+    <Wrapper>
+      <Title>임시데이터</Title>
+      <PostInfoWrapper>
+        <PostInfo>카테고리 임시</PostInfo>
+        <PostInfo>생성일 임시</PostInfo>
+      </PostInfoWrapper>
+      <Image src="/default-banner-image.png" />
+      <Content>임시데이터</Content>
+    </Wrapper>
+  );
 };
+
+const Content = styled.div``;
+const Image = styled.img`
+  width: 600px;
+  height: 300px;
+  object-fit: cover;
+`;
+
+const PostInfo = styled.div`
+  font-size: 15px;
+  color: gray;
+`;
+const PostInfoWrapper = styled.div`
+  width: 100%;
+  display: inline-flex;
+  justify-content: flex-end;
+  gap: 20px;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 900;
+`;
+const Wrapper = styled.div`
+  width: 600px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export default PostPage;

650480f587a25bd5d4dde6cb6d750487d5eac4e5
jiyoung <jyhan0309@gmail.com>
2024-10-29 10:18:56 +0900

    style: 아이콘 추가 (#5)

--- frontend/src/components/post/PostPage.tsx
+++ frontend/src/components/post/PostPage.tsx
@@ -2,21 +2,25 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { fetchPost } from "./postAPI";
 import styled from "styled-components";
+import { LuPencilLine } from "react-icons/lu";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 const PostPage = () => {
   const { id } = useParams<{ id: string }>();
   const fetchPostData = async () => {
     try {
       const data = await fetchPost(id as string);
-
+      // eslint-disable-next-line
       console.log(data);
     } catch (error) {
+      // eslint-disable-next-line
       console.error(error);
     }
   };
 
   useEffect(() => {
     fetchPostData();
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -28,9 +32,19 @@ const PostPage = () => {
       </PostInfoWrapper>
       <Image src="/default-banner-image.png" />
       <Content>임시데이터</Content>
+      <IconWrapper>
+        <LuPencilLine onClick={() => {}} size={30} />
+        <FaRegTrashCan onClick={() => {}} size={30} />
+      </IconWrapper>
     </Wrapper>
   );
 };
+
+const IconWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 15px;
+`;
 
 const Content = styled.div``;
 const Image = styled.img`

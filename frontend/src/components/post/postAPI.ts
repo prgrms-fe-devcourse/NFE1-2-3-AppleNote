2e8a03bebeff57bb37c3c296589dc3048ccfafae
jiyoung <jyhan0309@gmail.com>
2024-10-30 16:09:44 +0900

    feat: 포스트 삭제 API 생성 (#5)

--- frontend/src/components/post/postAPI.ts
+++ frontend/src/components/post/postAPI.ts
@@ -13,6 +13,13 @@ export type FetchPostPayload = {
   payload: Post;
 };
 
+export type DeletePostPayload = {
+  statueCode: number;
+  payload: {
+    isRemove: boolean;
+  };
+};
+
 /**
  * GET /posts/:id post data 가져오기
  * @requires Authorization Bearer {access-token}
@@ -22,6 +29,19 @@ export type FetchPostPayload = {
 export const fetchPost = async (id: string): Promise<FetchPostPayload> => {
   const URL = `/posts/${id}`;
   const { data } = await httpClient.get(URL);
+
+  return data;
+};
+
+/**
+ * DELETE /posts/:id post data 삭제하기
+ * @requires Authorization Bearer {access-token}
+ * @param id
+ * @returns isRemove 값 반환
+ */
+export const deletePost = async (id: string): Promise<DeletePostPayload> => {
+  const URL = `/posts/${id}`;
+  const { data } = await httpClient.delete(URL);
 
   return data;
 };

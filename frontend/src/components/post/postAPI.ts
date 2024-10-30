4a6dc407c938029ca7ef3d2d4b02e567b46c26d0
jiyoung <jyhan0309@gmail.com>
2024-10-30 14:45:24 +0900

    refactor: fetch post 코드리뷰 기반으로 리팩토링 (#5)

--- frontend/src/components/post/postAPI.ts
+++ frontend/src/components/post/postAPI.ts
@@ -1,4 +1,4 @@
-import { httpClient, BASE_URL } from "@common/api/fetch";
+import { httpClient } from "@common/api/fetch";
 
 export type Post = {
   postId: string;
@@ -8,27 +8,20 @@ export type Post = {
   authorId: string;
   category: string[];
 };
-export type PostResponse = {
+export type FetchPostPayload = {
   statusCode: number;
   payload: Post;
 };
-// 공통 API 호출 함수
-const apiCall = async <T, D = unknown>(
-  method: "get" | "post" | "put" | "delete",
-  endpoint: string,
-  data?: D
-): Promise<T> => {
-  try {
-    const response = await httpClient[method]<T>(`${BASE_URL}${endpoint}`, data);
 
-    return response.data;
-  } catch (error) {
-    // eslint-disable-next-line
-    console.error(`API 요청 실패: ${error}`);
-    throw new Error("서버와의 통신에 실패했습니다.");
-  }
+/**
+ * GET /posts/:id post data 가져오기
+ * @requires Authorization Bearer {access-token}
+ * @param id
+ * @returns post data
+ */
+export const fetchPost = async (id: string): Promise<FetchPostPayload> => {
+  const URL = `/posts/${id}`;
+  const { data } = await httpClient.get(URL);
+
+  return data;
 };
-
-// 포스트 데이터 가져오기
-export const fetchPost = async (id: string): Promise<PostResponse> =>
-  apiCall<PostResponse>("get", `/posts/${id}`);

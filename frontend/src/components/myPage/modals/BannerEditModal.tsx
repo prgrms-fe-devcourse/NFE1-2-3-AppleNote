dd119b94584dec44131b35fa0cc34ad3d745fe9c
unknown <yschoi0119@naver.com>
2024-11-04 17:01:07 +0900

    refactor: 이미지 변경 모달 일부 수정 (#43)

--- frontend/src/components/myPage/modals/BannerEditModal.tsx
+++ frontend/src/components/myPage/modals/BannerEditModal.tsx
@@ -11,25 +11,36 @@ interface ImageEditModalProps {
 
 //배너 이미지 수정 모달
 const ProfileEditModal: React.FC<ImageEditModalProps> = ({ onClose }) => {
-  const isValidImageUrl = (url: string) => {
-    return /^https?:\/\//i.test(url);
-  };
-
+  const [fileName, setFileName] = useState("");
   const [bannerImage, setNewImg] = useState("");
-  const handleConfirm = async () => {
-    if (!isValidImageUrl(bannerImage)) {
-      alert("유효하지 않은 이미지 형식입니다.");
-    } else {
-      const isChangeSuccessful = await changeBanner({ bannerImage });
 
-      if (isChangeSuccessful) {
-        alert("배너 이미지가 변경되었습니다.");
-        onClose();
-      } else {
-        alert("배너 이미지 변경에 실패했습니다.");
-      }
+  // 이미지 선택 핸들러
+  const handleImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const fileUrl = URL.createObjectURL(file);
+
+      setNewImg(fileUrl);
+      setFileName(file.name);
+    } else {
+      alert("파일을 선택하지 않았습니다.");
     }
   };
+
+  //선택한 이미지로 변경
+  const handleConfirm = async () => {
+    const isChangeSuccessful = await changeBanner({ bannerImage });
+
+    if (isChangeSuccessful) {
+      alert("배너 이미지가 변경되었습니다.");
+      onClose();
+    } else {
+      alert("배너 이미지 변경에 실패했습니다.");
+    }
+  };
+
+  // 기본이미지로 변경
   const handleDefault = async () => {
     const isChangeSuccessful = await changeBanner({ bannerImage: DEFAULT_PROFILE_IMAGE });
 
@@ -51,12 +62,12 @@ const ProfileEditModal: React.FC<ImageEditModalProps> = ({ onClose }) => {
               <img src={closeBtn} width="30px" />
             </CloseButton>
           </Header>
-          <Input
-            type="text"
-            value={bannerImage}
-            onChange={(e) => setNewImg(e.target.value.trim())}
-          />
+          <InputWrapper>
+            <Input type="file" id="file-input" accept="image/*" onChange={handleImg} />
 
+            <ButtonStyled htmlFor="file-input">파일 선택</ButtonStyled>
+            {fileName && <FileName>{fileName}</FileName>}
+          </InputWrapper>
           <ButtonWrapper>
             <Button onClick={handleConfirm}>변경</Button>
             <Button onClick={handleDefault}>삭제</Button>
@@ -101,14 +112,44 @@ const CloseButton = styled.button`
   cursor: pointer;
   padding: 0;
 `;
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+`;
+
 const Input = styled.input`
   width: 100%;
-  padding: 10px;
-  margin-top: 10px;
+  padding: 15px;
   border: 1px solid #ddd;
   border-radius: 5px;
   font-size: 1rem;
   box-sizing: border-box;
+  background-color: #f9f9f9;
+  cursor: pointer;
+  display: none;
+`;
+
+const ButtonStyled = styled.label`
+  padding: 15px 20px;
+  margin-left: 10px;
+  border: 1px solid #8f8f8f;
+  border-radius: 5px;
+  background-color: #7e7979;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: transparent;
+    color: black;
+  }
+`;
+const FileName = styled.p`
+  margin-top: 10px;
+  margin-left: 30px;
+  font-size: 0.9rem;
+  color: #555;
 `;
 const ButtonWrapper = styled.div`
   display: flex;

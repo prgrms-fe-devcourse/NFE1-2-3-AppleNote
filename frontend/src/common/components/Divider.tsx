fc6eb89cb2c73eb59b49a0100371086158e98879
sunglitter <kds0284@gmail.com>
2024-11-05 17:24:47 +0900

    fix: styled components 관련 오류 수정 (#6)

--- frontend/src/common/components/Divider.tsx
+++ frontend/src/common/components/Divider.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const Divider = styled.div<{ small?: boolean; width: number }>`
-  height: ${({ small }) => (small ? "clamp(4px, 1vw, 6px)" : "clamp(4px, 1.5vw, 7px)")};
+const Divider = styled.div<{ $small?: boolean; $width: number }>`
+  height: ${({ $small }) => ($small ? "clamp(4px, 1vw, 6px)" : "clamp(4px, 1.5vw, 7px)")};
   background-color: ${({ theme }) => theme.divider.background};
-  width: ${({ width }) => `${width}px`};
+  width: ${({ $width }) => `${$width}px`};
 `;
 
 export default Divider;
